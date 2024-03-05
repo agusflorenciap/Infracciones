ac7af654ce2e83ee971f1a4cdbef3c49a4e7cc3e
agustina <poseagustina@gmail.com>
2024-03-05 15:02:49 -0300

    Prueba

---
+++ infracciones/PlayerUI/FPagoMulta.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Printing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CapaNegocios;
+using Excepciones;
+
+namespace PlayerUI
+{
+    public partial class FPagoMulta : Form
+    {
+
+        private Agencia a;
+        private Vehiculo v;
+        private OrdenPago p;
+        public FPagoMulta(Agencia agencia)
+        {
+            a = agencia;
+            InitializeComponent();
+        }
+
+
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Aceptar_Click(object sender, EventArgs e) //siguiente
+        {
+            try
+            {
+         
+                Multa m = (Multa)listBoxMultas.SelectedItem;
+                if (m == null)
+                    throw new NoSeleccion();
+
+                FPagoMultaSiguiente fSiguiente = new FPagoMultaSiguiente(m);
+
+                fSiguiente.ShowDialog();
+                p = fSiguiente.OrdenGenerada;
+
+                a.registrarPago(p);
+                m.Pagada = true; //cambio variable bool
+                a.actualizarMulta(m); //actualizo el registro en bd
+
+                this.Close();
+            }
+            catch (NoSeleccion ex)
+            {
+                MessageBox.Show(ex.Message);
+                listBoxMultas.Focus();
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+
+        }
+
+        private void buttonBuscar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string dom = textBoxDominio.Text;
+
+                if (dom.Length==0)
+                    throw new BlancoException();
+
+                v = a.buscameVehiculo(dom);
+
+                if (v == null)
+                    throw new InexistenciaVehiculo();
+
+                else //existe. lleno el listbox.
+                {
+
+                    label2.Visible = true;
+                    listBoxMultas.Visible = true;
+                    
+
+                    List<Multa> listaOriginal = v.ListaMultas;
+                   /* List<Multa> listaFiltrada = new List<Multa>();
+                    int i = 0;
+
+                    while (i <listaOriginal.Count) //recorro la lista y dejo solo a las multas que no estan pagas
+                    {
+                        Multa m = listaOriginal.ElementAt(i);
+                        bool paga = m.Pagada;
+
+                        if (paga)
+                            listaFiltrada.Add(m);
+
+                        i++;
+                    }
+                    me faltaba incrementar i. y este método va en la capa de negocios (vehiculo)
+                    */
+                    /*
+                    listBoxMultas.DataSource = null;
+                    listBoxMultas.DataSource = listaFiltrada;
+                    listBoxMultas.ClearSelected();
+                    */
+
+                    listBoxMultas.DataSource = null;
+                    listBoxMultas.DataSource = listaOriginal;
+                    listBoxMultas.ClearSelected();
+                }
+            }
+
+            catch (InexistenciaVehiculo ex)
+            {
+                MessageBox.Show(ex.Message);
+                textBoxDominio.Focus();
+            }
+
+            catch (BlancoException ex)
+            {
+                MessageBox.Show(ex.Message);
+                textBoxDominio.Focus();
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
---
+++ infracciones/PlayerUI/FPagoMulta.designer.cs
@@ -0,0 +1,199 @@
+﻿namespace PlayerUI
+{
+    partial class FPagoMulta
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.Cerrar = new System.Windows.Forms.Button();
+            this.Cancelar = new System.Windows.Forms.Button();
+            this.Aceptar = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxDominio = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.buttonBuscar = new System.Windows.Forms.Button();
+            this.listBoxMultas = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(42)))), ((int)(((byte)(83)))));
+            this.label1.Location = new System.Drawing.Point(257, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(182, 25);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Formulario de Pago";
+            // 
+            // Cerrar
+            // 
+            this.Cerrar.FlatAppearance.BorderSize = 0;
+            this.Cerrar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(42)))), ((int)(((byte)(83)))));
+            this.Cerrar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Cerrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Cerrar.ForeColor = System.Drawing.Color.LightGray;
+            this.Cerrar.Location = new System.Drawing.Point(0, 0);
+            this.Cerrar.Name = "Cerrar";
+            this.Cerrar.Size = new System.Drawing.Size(25, 25);
+            this.Cerrar.TabIndex = 14;
+            this.Cerrar.Text = "X";
+            this.Cerrar.UseVisualStyleBackColor = true;
+            this.Cerrar.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Cancelar
+            // 
+            this.Cancelar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.Cancelar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(22)))), ((int)(((byte)(34)))));
+            this.Cancelar.FlatAppearance.BorderSize = 0;
+            this.Cancelar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Cancelar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Cancelar.ForeColor = System.Drawing.Color.LightGray;
+            this.Cancelar.Location = new System.Drawing.Point(488, 359);
+            this.Cancelar.Name = "Cancelar";
+            this.Cancelar.Size = new System.Drawing.Size(150, 40);
+            this.Cancelar.TabIndex = 16;
+            this.Cancelar.Text = "Cancelar";
+            this.Cancelar.UseVisualStyleBackColor = false;
+            this.Cancelar.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // Aceptar
+            // 
+            this.Aceptar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.Aceptar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(42)))), ((int)(((byte)(83)))));
+            this.Aceptar.FlatAppearance.BorderSize = 0;
+            this.Aceptar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Aceptar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Aceptar.ForeColor = System.Drawing.Color.LightGray;
+            this.Aceptar.Location = new System.Drawing.Point(332, 359);
+            this.Aceptar.Name = "Aceptar";
+            this.Aceptar.Size = new System.Drawing.Size(150, 40);
+            this.Aceptar.TabIndex = 15;
+            this.Aceptar.Text = "Siguiente";
+            this.Aceptar.UseVisualStyleBackColor = false;
+            this.Aceptar.Click += new System.EventHandler(this.Aceptar_Click);
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(86, 112);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(151, 17);
+            this.label2.TabIndex = 19;
+            this.label2.Text = "Seleccione una Multa: ";
+            this.label2.Visible = false;
+            // 
+            // textBoxDominio
+            // 
+            this.textBoxDominio.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxDominio.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxDominio.Location = new System.Drawing.Point(200, 72);
+            this.textBoxDominio.Name = "textBoxDominio";
+            this.textBoxDominio.Size = new System.Drawing.Size(368, 26);
+            this.textBoxDominio.TabIndex = 27;
+            // 
+            // label6
+            // 
+            this.label6.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(86, 72);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(144, 17);
+            this.label6.TabIndex = 26;
+            this.label6.Text = "Dominio del Vehículo:";
+            // 
+            // buttonBuscar
+            // 
+            this.buttonBuscar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonBuscar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(42)))), ((int)(((byte)(83)))));
+            this.buttonBuscar.FlatAppearance.BorderSize = 0;
+            this.buttonBuscar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonBuscar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonBuscar.ForeColor = System.Drawing.Color.LightGray;
+            this.buttonBuscar.Location = new System.Drawing.Point(591, 102);
+            this.buttonBuscar.Name = "buttonBuscar";
+            this.buttonBuscar.Size = new System.Drawing.Size(61, 27);
+            this.buttonBuscar.TabIndex = 28;
+            this.buttonBuscar.Text = "Buscar";
+            this.buttonBuscar.UseVisualStyleBackColor = false;
+            this.buttonBuscar.Click += new System.EventHandler(this.buttonBuscar_Click);
+            // 
+            // listBoxMultas
+            // 
+            this.listBoxMultas.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBoxMultas.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(22)))), ((int)(((byte)(34)))));
+            this.listBoxMultas.ForeColor = System.Drawing.Color.LightGray;
+            this.listBoxMultas.FormattingEnabled = true;
+            this.listBoxMultas.Location = new System.Drawing.Point(33, 164);
+            this.listBoxMultas.Name = "listBoxMultas";
+            this.listBoxMultas.Size = new System.Drawing.Size(639, 186);
+            this.listBoxMultas.TabIndex = 29;
+            this.listBoxMultas.Visible = false;
+            // 
+            // FPagoMulta
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(30)))), ((int)(((byte)(45)))));
+            this.ClientSize = new System.Drawing.Size(684, 431);
+            this.Controls.Add(this.listBoxMultas);
+            this.Controls.Add(this.buttonBuscar);
+            this.Controls.Add(this.textBoxDominio);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Cancelar);
+            this.Controls.Add(this.Aceptar);
+            this.Controls.Add(this.Cerrar);
+            this.Controls.Add(this.label1);
+            this.Name = "FPagoMulta";
+            this.Text = "Form3";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Cerrar;
+        private System.Windows.Forms.Button Cancelar;
+        private System.Windows.Forms.Button Aceptar;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxDominio;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button buttonBuscar;
+        private System.Windows.Forms.ListBox listBoxMultas;
+    }
+}
